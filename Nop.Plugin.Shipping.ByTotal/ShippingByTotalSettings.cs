4045df3354296761ce3f1fe8dd31b7fa3c25e3c0
mb <none@none>
2017-11-15 08:58:42 -0700

    version 1.24 (supported nopCommerce version: 4.00)

--- Nop.Plugin.Shipping.ByTotal/ShippingByTotalSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿using Nop.Core.Configuration;
-
-namespace Nop.Plugin.Shipping.ByTotal
-{
-    public class ShippingByTotalSettings : ISettings
-    {
-        public bool LimitMethodsToCreated { get; set; }
-    }
-}
